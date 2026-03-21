e1b716a9e0014f88e408669be8ad1e12270f00c1
Tiffany Conroy <tiffany.conroy@gmail.com>
2026-03-21 13:22:51 +0100

    feat: add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/kanquer/',
   server: { historyApiFallback: true },
   test: {
     globals: true,
